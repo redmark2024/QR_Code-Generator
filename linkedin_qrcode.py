63f779cdeeb8b8c5188d9b16d70d97048b5c1a55
RedMark <mahatapritideep60@gmail.com>
2025-01-15 10:05:30 +0530

    Update and rename yt_code.py to linkedin_qrcode.py

--- linkedin_qrcode.py
+++ linkedin_qrcode.py
@@ -1,6 +1,6 @@
 import qrcode 
 
-url="" #add your url (youtube,facebook,instagram,github etc...)
+url="https://www.linkedin.com/in/pritideep-mahata/" #add your url (youtube,facebook,instagram,github etc...)
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
